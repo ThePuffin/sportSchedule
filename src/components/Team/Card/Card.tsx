91574fcc0b0e993778cb4582909f477ff953c7d9
ThePuffin <ballutvincent@gmail.com>
2024-06-01 17:16:30 +0200

    show calendar

--- src/components/Team/Card/Card.tsx
+++ src/components/Team/Card/Card.tsx
@@ -1,6 +1,8 @@
 import './Card.css';
+import './colorsTeam.css';
 import React, { Component, useEffect } from 'react';
 import { teamSelected } from '../../../store/store.js';
+import currentSeason from '../../../../temporaryData/currentSeason.json';
 
 export default class TeamCard extends Component {
   constructor(props) {
@@ -11,9 +13,12 @@ export default class TeamCard extends Component {
       i: props.i,
       activeTeams: props.activeTeams,
       id: team.id,
+      showAway: false,
+      showHome: true,
+      gamesData: [],
     };
   }
-  divId = `${Math.random()}`;
+
   subscription = undefined;
 
   subscribeToTeamSelected() {
@@ -24,7 +29,8 @@ export default class TeamCard extends Component {
 
       if (newSelectionId && this.state.id !== newSelectionId) {
         const newTeam = this.state.activeTeams.find((activeTeam) => activeTeam.id === newSelectionId);
-        this.setState({ team: newTeam });
+        this.getGames(newSelectionId);
+        this.setState({ team: newTeam, id: newTeam.id });
       }
     });
 
@@ -33,6 +39,7 @@ export default class TeamCard extends Component {
   }
 
   componentDidMount() {
+    this.getGames(this.state.id);
     this.subscribeToTeamSelected();
   }
 
@@ -42,16 +49,72 @@ export default class TeamCard extends Component {
       this.subscription.unsubscribe();
     }
   }
+  getGames(teamSelectedId) {
+    const { games } = currentSeason[teamSelectedId];
+    const newGamesData = games.map((game) => {
+      return {
+        awayTeamId: game.awayTeam.abbrev,
+        awayTeamShort: game.awayTeam.abbrev,
+        homeTeamId: game.homeTeam.abbrev,
+        homeTeamShort: game.homeTeam.abbrev,
+        arenaName: game.venue?.default || '',
+        gameDate: new Date(game.gameDate).toISOString().split('T')[0],
+        teamSelectedId,
+        timestampDate: new Date(game.gameDate).getTime(),
+        show:
+          (game.homeTeam.abbrev === teamSelectedId && this.state.showHome) ||
+          (game.awayTeam.abbrev === teamSelectedId && this.state.showAway),
+        selectedTeam: game.homeTeam.abbrev === teamSelectedId,
+      };
+    });
+    this.setState({ gamesData: newGamesData });
+  }
 
   render() {
+    const hideDate = false;
+    const dateSelected = new Date();
+
     return (
-      <div id={this.state.team.id} className="App">
-        <h2>
-          {this.state.i + 1}
-          <img src={this.state.team.teamLogo} alt={this.state.team.value} />
-          {this.state.team.label}
-        </h2>
-      </div>
+      // <div id={this.state.team.id} className="App">
+      //   <h2>
+      //     {this.state.i + 1}
+      //     <img src={this.state.team.teamLogo} alt={this.state.team.value} />
+      //     {this.state.team.label}
+      //   </h2>
+      //   <p>{this.state.gamesData[0]?.homeTeamId}</p>
+      //   vs
+      //   <p>{this.state.gamesData[0]?.awayTeamId}</p>
+      // </div>
+
+      // TODO: uncomment when columns are ok
+      this.state.gamesData &&
+      this.state.gamesData.map((data) => {
+        return (
+          <div
+            className={
+              data.arenaName && data.show
+                ? data.selectedTeam
+                  ? `card t${data.homeTeamId}`
+                  : `card awayGame`
+                : 'card unclickable'
+            }
+          >
+            <div className={dateSelected >= 0 ? 'selected' : ''}>
+              <div className={data.show ? 'ext-box' : 'whiteCard'}>
+                <div>
+                  <p className={hideDate ? 'cardText hideDate' : 'cardText'}>{data.gameDate}</p>
+                </div>
+                <h4 className="cardText">{data.awayTeamShort}</h4>
+                {!data.show ? <img src={this.state.team.teamLogo} alt={this.state.team.value} /> : ''}
+                <p className="cardText vs">vs</p>
+                <h4 className="cardText">{data.homeTeamShort}</h4>
+                {data.show ? <img src={this.state.team.teamLogo} alt={this.state.team.value} /> : ''}
+                <p className="cardText arena"> {data.arenaName}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })
     );
   }
 }
