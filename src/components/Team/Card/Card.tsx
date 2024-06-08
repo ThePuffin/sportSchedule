d4151ab5d74605be4a46dd918181661e3477c1f9
ThePuffin <ballutvincent@gmail.com>
2024-06-08 10:15:06 +0200

    refactor

--- src/components/Team/Card/Card.tsx
+++ src/components/Team/Card/Card.tsx
@@ -3,6 +3,7 @@ import './colorsTeam.css';
 import React, { Component, useEffect } from 'react';
 import { teamSelected } from '../../../store/store.js';
 import currentSeason from '../../../../temporaryData/currentSeason.json';
+import axios from 'axios';
 
 export default class TeamCard extends Component {
   constructor(props) {
@@ -38,7 +39,7 @@ export default class TeamCard extends Component {
     this.subscription = subscription;
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.getGames(this.state.id);
     this.subscribeToTeamSelected();
   }
@@ -50,80 +51,81 @@ export default class TeamCard extends Component {
     }
   }
   getGames(teamSelectedId) {
-    const { games } = currentSeason[teamSelectedId];
-    const newGamesData = games.map((game) => {
-      return {
-        awayTeamId: game.awayTeam.abbrev,
-        awayTeamShort: game.awayTeam.abbrev,
-        homeTeamId: game.homeTeam.abbrev,
-        homeTeamShort: game.homeTeam.abbrev,
-        arenaName: game.venue?.default || '',
-        gameDate: new Date(game.gameDate).toISOString().split('T')[0],
-        teamSelectedId,
-        timestampDate: new Date(game.gameDate).getTime(),
-        show:
-          (game.homeTeam.abbrev === teamSelectedId && this.state.showHome) ||
-          (game.awayTeam.abbrev === teamSelectedId && this.state.showAway),
-        selectedTeam: game.homeTeam.abbrev === teamSelectedId,
-      };
-    });
-    this.setState({ gamesData: newGamesData });
+    try {
+      const { games } = currentSeason[teamSelectedId];
+
+      const newGamesData = games.map((game) => {
+        return {
+          awayTeamId: game.awayTeam.abbrev,
+          awayTeamShort: game.awayTeam.abbrev,
+          homeTeamId: game.homeTeam.abbrev,
+          homeTeamShort: game.homeTeam.abbrev,
+          arenaName: game.venue?.default || '',
+          gameDate: new Date(game.gameDate).toISOString().split('T')[0],
+          teamSelectedId,
+          timestampDate: new Date(game.gameDate).getTime(),
+          show:
+            (game.homeTeam.abbrev === teamSelectedId && this.state.showHome) ||
+            (game.awayTeam.abbrev === teamSelectedId && this.state.showAway),
+          selectedTeam: game.homeTeam.abbrev === teamSelectedId,
+        };
+      });
+      this.setState({ gamesData: newGamesData });
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 
   render() {
     const hideDate = false;
     const dateSelected = new Date();
 
-         if (this.state.team.id &&
-      this.state.gamesData.length ){
-          return (
-            <div id={this.state.team.id} className="App">
-              <h2>
-                <img src={this.state.team.teamLogo} alt={this.state.team.value} />
-                {this.state.team.label}
-              </h2>
-              <h5>first game: </h5>
-              <p>{this.state.gamesData[0]?.homeTeamId}</p>
-              vs
-              <p>{this.state.gamesData[0]?.awayTeamId}</p>
-            </div>
+    if (this.state.team.id && this.state.gamesData.length) {
+      const lastGame = this.state.gamesData.length - 1;
+      return (
+        <div id={this.state.team.id} className="App">
+          <h2>
+            <img src={this.state.team.teamLogo} alt={this.state.team.value} />
+            {this.state.team.label}
+          </h2>
+          <h5>Last game: </h5>
+          <p>{this.state.gamesData?.[lastGame]?.homeTeamId}</p>
+          vs
+          <p>{this.state.gamesData?.[lastGame]?.awayTeamId}</p>
+        </div>
 
-            // TODO: uncomment when columns are ok
-            // this.state.gamesData &&
-            // this.state.gamesData.map((data) => {
-            //   return (
-            //     <div
-            //       className={
-            //         data.arenaName && data.show
-            //           ? data.selectedTeam
-            //             ? `card t${data.homeTeamId}`
-            //             : `card awayGame`
-            //           : 'card unclickable'
-            //       }
-            //     >
-            //       <div className={dateSelected >= 0 ? 'selected' : ''}>
-            //         <div className={data.show ? 'ext-box' : 'whiteCard'}>
-            //           <div>
-            //             <p className={hideDate ? 'cardText hideDate' : 'cardText'}>{data.gameDate}</p>
-            //           </div>
-            //           <h4 className="cardText">{data.awayTeamShort}</h4>
-            //           {!data.show ? <img src={this.state.team.teamLogo} alt={this.state.team.value} /> : ''}
-            //           <p className="cardText vs">vs</p>
-            //           <h4 className="cardText">{data.homeTeamShort}</h4>
-            //           {data.show ? <img src={this.state.team.teamLogo} alt={this.state.team.value} /> : ''}
-            //           <p className="cardText arena"> {data.arenaName}</p>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   );
-            // })
-          );
-      } 
-      else {
-        return (
-          <p>wait for it...</p>
-        )
-      }
-  
+        // TODO: uncomment when columns are ok
+        // this.state.gamesData &&
+        // this.state.gamesData.map((data) => {
+        //   return (
+        //     <div
+        //       className={
+        //         data.arenaName && data.show
+        //           ? data.selectedTeam
+        //             ? `card t${data.homeTeamId}`
+        //             : `card awayGame`
+        //           : 'card unclickable'
+        //       }
+        //     >
+        //       <div className={dateSelected >= 0 ? 'selected' : ''}>
+        //         <div className={data.show ? 'ext-box' : 'whiteCard'}>
+        //           <div>
+        //             <p className={hideDate ? 'cardText hideDate' : 'cardText'}>{data.gameDate}</p>
+        //           </div>
+        //           <h4 className="cardText">{data.awayTeamShort}</h4>
+        //           {!data.show ? <img src={this.state.team.teamLogo} alt={this.state.team.value} /> : ''}
+        //           <p className="cardText vs">vs</p>
+        //           <h4 className="cardText">{data.homeTeamShort}</h4>
+        //           {data.show ? <img src={this.state.team.teamLogo} alt={this.state.team.value} /> : ''}
+        //           <p className="cardText arena"> {data.arenaName}</p>
+        //         </div>
+        //       </div>
+        //     </div>
+        //   );
+        // })
+      );
+    } else {
+      return <p>wait for it...</p>;
+    }
   }
 }
--- src/components/Team/Selector/Selector.tsx
+++ src/components/Team/Selector/Selector.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Select from "react-select";
-import { teamSelected } from "../../../store/store.js";
+import React from 'react';
+import Select from 'react-select';
+import { teamSelected } from '../../../store/store.js';
 
-export default function Selector({ teamsSelectedIds, activeTeams, i }) {
+export default function Selector({ teamsSelectedIds, activeTeams, i, teamSelectedId }) {
   let teamsId = teamSelected.get();
-  const teamId = teamsSelectedIds[i];
+  const teamId = teamSelectedId;
   if (teamsId[0] === undefined) {
     teamSelected.set(teamsSelectedIds);
   }
 
   const teamData = activeTeams.find((team) => team.value === teamId);
-  const { label = "", teamLogo } = teamData;
+  const { label = '', teamLogo } = teamData;
 
   const changeTeam = async (event) => {
     teamsId = [...teamSelected.get()];
@@ -18,19 +18,15 @@ export default function Selector({ teamsSelectedIds, activeTeams, i }) {
 
     teamSelected.set(teamsId);
   };
-  const filtredTeam = activeTeams.filter(
-    (team) => !teamsSelectedIds.includes(team.value),
-  );
+
+  //TODO: to use later to avoid to select same team multi times (linked it to the store or state ???)
+  // const filtredTeam = activeTeams.filter(
+  //   (team) => !teamsSelectedIds.includes(team.value),
+  // );
 
   return (
     <div className="App">
-      <Select
-        defaultValue={teamId}
-        placeholder={label}
-        onChange={changeTeam}
-        isSearchable
-        options={filtredTeam}
-      />
+      <Select defaultValue={teamId} placeholder={label} onChange={changeTeam} isSearchable options={activeTeams} />
     </div>
   );
 }
