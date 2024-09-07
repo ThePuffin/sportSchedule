ab70b17d7bda81af12c8b96b5293949d94eded7d
ThePuffin <ballutvincent@gmail.com>
2024-09-07 18:07:17 +0200

    fix nba schedule

---
+++ src/utils/nbaSchedule.ts
@@ -0,0 +1,66 @@
+import { League } from './enum.ts';
+const venueTimezone = 'America/New_York';
+import { readableDate } from '../utils/date.js';
+let gameDates = [];
+
+export const getNBASchedule = async () => {
+  const scheduleEndpoint = 'https://cdn.nba.com/static/json/staticData/scheduleLeagueV2.json';
+  const fetchSchedule = await fetch(scheduleEndpoint);
+  const { leagueSchedule } = await fetchSchedule.json();
+  if (leagueSchedule.gameDates.length) {
+    gameDates = leagueSchedule.gameDates;
+  }
+};
+
+export const filterGamesByTeam = (team, value) => {
+  const teamTricodeMap = {
+    SA: 'SAS',
+    NO: 'NOP',
+    UTAH: 'UTA',
+  };
+  const inverseTeamTricodeMap = {
+    SAS: 'SA',
+    NOP: 'NO',
+    UTA: 'UTAH',
+  };
+
+  const teamTricode = teamTricodeMap[team] || team;
+
+  return gameDates
+    .map(({ gameDate, games }) => {
+      const filterGame = games.filter(
+        (game) => game.homeTeam.teamTricode === teamTricode || game.awayTeam.teamTricode === teamTricode
+      );
+      if (filterGame.length) {
+        const gameDateRead = readableDate(new Date(gameDate));
+        const { gameCode, arenaName, homeTeam, awayTeam } = filterGame[0];
+        const awayAbbrev = inverseTeamTricodeMap[awayTeam.teamTricode] || awayTeam.teamTricode;
+        const homeAbbrev = inverseTeamTricodeMap[homeTeam.teamTricode] || homeTeam.teamTricode;
+
+        const dateString = '2025-04-13T13:00:00Z';
+        const date = new Date(dateString);
+
+        const hourStart = date.getHours();
+        const minStart = date.getMinutes();
+
+        return {
+          uniqueId: gameCode,
+          arenaName: arenaName || '',
+          awayTeamId: awayAbbrev,
+          awayTeam: `${awayTeam.teamCity} ${awayTeam.teamName}`,
+          awayTeamShort: awayAbbrev,
+          homeTeam: `${homeTeam.teamCity} ${homeTeam.teamName}`,
+          homeTeamId: homeAbbrev,
+          homeTeamShort: homeAbbrev,
+          gameDate: gameDateRead,
+          teamSelectedId: value,
+          show: homeAbbrev === team,
+          selectedTeam: homeAbbrev === team,
+          league: League.NBA,
+          venueTimezone,
+          timeStart: `${hourStart}:${minStart}`,
+        };
+      }
+    })
+    .filter((game) => game?.uniqueId);
+};
