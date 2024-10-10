d460a08093826c00645b414da54733dba6f7b1b8
ThePuffin <ballutvincent@gmail.com>
2024-10-10 21:09:51 +0200

    update nhl calendar

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,9 +1,9 @@
 export const readableDate = (date) => {
-  return new Date(date).toISOString().split("T")[0];
+  return new Date(date).toISOString().split('T')[0];
 };
 
 export const isExpiredData = (date) => {
-  date = date ?? new Date("2020-02-20");
+  date = date ?? new Date('2020-02-20');
   const expirationDay = 1;
   const now = new Date();
   const differenceInMilliseconds = now.getTime() - new Date(date).getTime();
@@ -12,18 +12,10 @@ export const isExpiredData = (date) => {
 };
 
 export const getHourGame = (startTimeUTC, venueUTCOffset) => {
-  const timeToRemove = Number(
-    venueUTCOffset.replace(":", ".").replace("-", ""),
-  );
+  const timeToRemove = Number(venueUTCOffset.replace(':', '.').replace('-', ''));
   const starTime = new Date(startTimeUTC);
   const getCorrectDate = starTime.setHours(starTime.getHours() - timeToRemove);
-  const hourStart = new Date(getCorrectDate)
-    .getUTCHours()
-    .toString()
-    .padStart(2, "0");
-  const minStart = new Date(getCorrectDate)
-    .getMinutes()
-    .toString()
-    .padStart(2, "0");
+  const hourStart = new Date(getCorrectDate).getUTCHours().toString().padStart(2, '0');
+  const minStart = new Date(getCorrectDate).getMinutes().toString().padStart(2, '0');
   return `${hourStart}:${minStart}`;
 };
