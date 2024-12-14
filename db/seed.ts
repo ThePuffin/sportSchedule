5e8bc392dfb94385eb1543aba1db7a39333cde84
ThePuffin <ballutvincent@gmail.com>
2024-12-14 11:48:31 +0100

    avoid error on adding game on DB

--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,12 @@
-import { Games, Teams, db } from "astro:db";
-import lastAllTeamsMLB from "../temporaryData/allTeamsMLB.json";
-import lastAllTeamsNBA from "../temporaryData/allTeamsNBA.json";
-import lastAllTeamsNFL from "../temporaryData/allTeamsNFL.json";
-import lastAllTeamsNHL from "../temporaryData/allTeamsNHL.json";
-import lastGamesMLB from "../temporaryData/updatecurrentSeasonMLB.json";
-import lastGamesNBA from "../temporaryData/updatecurrentSeasonNBA.json";
-import lastGamesNFL from "../temporaryData/updatecurrentSeasonNFL.json";
-import lastGamesNHL from "../temporaryData/updatecurrentSeasonNHL.json";
+import { Games, Teams, db } from 'astro:db';
+import lastAllTeamsMLB from '../temporaryData/allTeamsMLB.json';
+import lastAllTeamsNBA from '../temporaryData/allTeamsNBA.json';
+import lastAllTeamsNFL from '../temporaryData/allTeamsNFL.json';
+import lastAllTeamsNHL from '../temporaryData/allTeamsNHL.json';
+import lastGamesMLB from '../temporaryData/updatecurrentSeasonMLB.json';
+import lastGamesNBA from '../temporaryData/updatecurrentSeasonNBA.json';
+import lastGamesNFL from '../temporaryData/updatecurrentSeasonNFL.json';
+import lastGamesNHL from '../temporaryData/updatecurrentSeasonNHL.json';
 
 // https://astro.build/db/seed
 export default async function insertTeams() {
@@ -25,11 +25,11 @@ export default async function insertTeams() {
   };
 
   await db.insert(Teams).values(allTeams);
-  for (const [team, games] of Object.entries(lastGames)) {
-    if (games.length) {
+  for (const [team = 'no team', games = []] of Object.entries(lastGames)) {
+    if (games?.length) {
       await db.insert(Games).values(games);
     } else {
-      console.log("No games found for", team);
+      console.log('No games found for', team);
     }
   }
 }
